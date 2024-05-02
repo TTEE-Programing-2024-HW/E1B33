e6cf75a183ebbef70ebaff998794edca9ea5b0ee
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-05-02 10:41:22 +0800

    個人風格畫面

---
+++ homework2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	printf("-------------------\n");
+	printf("|                 |\n");
+	printf("|     系所:電機系 |\n");
+	printf("|                 |\n");
+	printf("|     班級:E1B    |\n");
+	printf("|                 |\n");
+	printf("|     座號:33     |\n");
+	printf("|                 |\n");
+	printf("|     姓名:張宸瑋 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    程式設計HW1  |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    2024/4/1     |\n");
+	printf("-------------------\n");
+	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
+	system("cls");//清除畫面 
+	int password;
+	printf("輸入4個數字密碼:");
+	scanf("%d",&password);
+	if(password!=2024){
+		printf("錯誤");
+		scanf("%d,&password);
+		
+		
+	}
+	}
+	
