1287ef730e50c1c0af1c2b39c156d1b7607ece2d
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-05-16 10:23:21 +0800

    新增檔案 第一題

---
+++ homework3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	printf("-------------------\n");
+	printf("|                 |\n");
+	printf("|     系所:電機系 |\n");
+	printf("|                 |\n");
+	printf("|     班級:E1B    |\n");
+	printf("|                 |\n");
+	printf("|     座號:33     |\n");
+	printf("|                 |\n");
+	printf("|     姓名:張宸瑋 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    程式設計HW3  |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    2024/5/16    |\n");
+	printf("-------------------\n");
+	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
+	system("cls");//清除畫面 
