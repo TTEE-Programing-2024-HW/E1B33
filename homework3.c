4d078cd70438790d6cff5f6c54c08558e3dd7c23
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-05-16 10:52:15 +0800

    第二題

--- homework3.c
+++ homework3.c
@@ -21,3 +21,33 @@ int main(void){
 	printf("-------------------\n");
 	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
 	system("cls");//清除畫面 
+	int password,count;
+	printf("輸入4個數字密碼:");
+	scanf("%d",&password);
+	for(;;){
+		if(count==2){
+			system("pause");
+			return 0;
+		}
+		if(password==2024){
+			break;
+		}else{
+		printf("錯誤再輸入一次密碼");
+		scanf("%d",&password);
+		count++;
+	}
+}
+	system("pause");
+	system("cls");
+	for(;;){
+	
+		printf("--------[Booking System]---------\n");
+		printf("| a. Available seats            \n|");
+		printf("| b. Arrange for you            \n|");
+		printf("| c. Choose by yourself         \n|");
+		printf("| d. Exit                       \n|");
+		printf("---------------------------------\n");
+}
+}
+	
+		
