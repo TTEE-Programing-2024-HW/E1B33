bb402f74d370bfd161e8b298e94fe3ca8bc68752
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-05-16 11:33:43 +0800

    第三題

--- homework3.c
+++ homework3.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
+#include<time.h>
+int x,y,i,j;
+char seat[10][10]={{'\\','1','2','3','4','5','6','7','8','9'},
+					{'9','-','-','-','-','-','-','-','-','-'},
+					{'8','-','-','-','-','-','-','-','-','-'},
+					{'7','-','-','-','-','-','-','-','-','-'},
+					{'6','-','-','-','-','-','-','-','-','-'},
+					{'5','-','-','-','-','-','-','-','-','-'},
+					{'4','-','-','-','-','-','-','-','-','-'},
+					{'3','-','-','-','-','-','-','-','-','-'},
+					{'2','-','-','-','-','-','-','-','-','-'},
+					{'1','-','-','-','-','-','-','-','-','-'}};
+void choise_a(char seat[10][10]);
 int main(void){
+	for(i=0;i<10;i++){
+	srand(time(NULL));
+	x=rand()%9+1;
+	y=rand()%9+1;
+	for(;;){
+		if(seat[x][y]=='*'){
+				x=rand()%9+1;
+				y=rand()%9+1;
+		}else{
+			break;
+		}
+		
+		
+	} 
+	seat[x][y]='*';
+}
 	printf("-------------------\n");
 	printf("|                 |\n");
 	printf("|     系所:電機系 |\n");
@@ -22,6 +52,7 @@ int main(void){
 	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
 	system("cls");//清除畫面 
 	int password,count;
+	char ch,g;
 	printf("輸入4個數字密碼:");
 	scanf("%d",&password);
 	for(;;){
@@ -40,14 +71,43 @@ int main(void){
 	system("pause");
 	system("cls");
 	for(;;){
-	
 		printf("--------[Booking System]---------\n");
 		printf("| a. Available seats            \n|");
 		printf("| b. Arrange for you            \n|");
 		printf("| c. Choose by yourself         \n|");
 		printf("| d. Exit                       \n|");
 		printf("---------------------------------\n");
+		ch=getch();
+		switch (ch)
+		case 'a':
+		case 'A':
+			choise_a(seat);
+			break;
+		case 'b':
+		case 'B':
+			printf("Continue?");
+			scanf(" %c",&g);
+			if(g=='Y'||g=='y'){
+			
+			system("CLS");
+			break;
+		}
+		else if(g=='N'||g=='n'){
+			system("pause");
+			return 0;
+			
+}else{
+			printf("錯誤重新輸入");
+		}
 }
+void choise_a(char seat[10][10]){
+	for(i=0;i<10;i++){
+		for(j=0;j<10;j++){
+			printf(" %c",seat[i][j]);
+		}
+		printf("\n");
+	}
+	
 }
 	
 		
