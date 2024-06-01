a004097107f4dd08df915181c713c38b737ecbb1
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-06-01 11:01:51 +0800

    第三題

--- homework4.c
+++ homework4.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+#define MAX 20
+void choise_a();
+void choise_b();
+void choise_c();
+void choise_d();
+int choise_e(); 
+struct info{
+	char name[MAX];
+	char id[6];
+	int math,phy,eng;
+};
+struct info student[MAX];
+int temp=0,rank[MAX];
+float avg[MAX]={0};
 int main(void){
 		printf("-------------------\n");
 	printf("|                 |\n");
@@ -21,7 +37,7 @@ int main(void){
 	printf("-------------------\n");
 	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
 	system("cls");//清除畫面 
-		int password,count;
+		int password,count,n,i;
 		char ch,g;
 			printf("輸入4個數字密碼:");
 	scanf("%d",&password);
@@ -50,7 +66,115 @@ int main(void){
 		printf("---------------------------------\n");
 		ch=getch();
 			switch (ch){
-				
+		case 'a':
+		case 'A':
+			choise_a();
+			break;
+		case 'b':
+		case 'B':
+			choise_b();
+			break;
+		case 'c':
+		case 'C':
+			choise_c();
+			break;
+		case 'd':
+		case 'D':
+			choise_d();
+			break;
+		case 'E':
+		case 'e':	
+		    if(choise_e==0){
+		    	return 0;
+			}else{
+				break;
 			}
+			
+		}
 }
 }
+void choise_a(void){
+	int i,j,n,int_temp;
+	float float_temp,avg_temp[MAX]={0};
+	system("cls");
+	for(;;){
+		printf("輸入n(5-10):");
+		fflush(stdin);
+		scanf("%d",&n);
+		if(5<=n&&n<=10)
+		break;
+	}
+	for(i=0;i<n;i++){
+		printf("%d學生\n",i+1);
+		scanf("%s %s %d %d %d",&student[temp+i].name,&student[temp+i].id,&student[temp+i].math,&student[temp+i].phy,&student[temp+i].eng);
+		for(;;){
+			if(strlen(student[temp+i].id)==6){
+				break;
+			}else{
+				printf("錯誤");
+				fflush(stdin);
+				scanf("%s",&student[temp+i].id);
+				continue;
+			}
+		}
+		for(;;){
+			if(0<=student[temp+i].math&&student[temp+i].math<=100){
+				break;
+			}else{
+				printf("wrong(0-100)");
+				fflsuh(stdin);
+				scanf("%d",&student[temp+i].math);
+				continue;
+			}
+		}
+		for(;;){
+			if(0<=student[temp+i].phy&&student[temp+i].phy<=100){
+				break;
+			}else{
+				printf("wrong(0-100)");
+				fflsuh(stdin);
+				scanf("%d",&student[temp+i].phy);
+				continue;}
+			}
+			for(;;){
+			if(0<=student[temp+i].eng&&student[temp+i].eng<=100){
+				break;
+			}else{
+				printf("wrong(0-100)");
+				fflsuh(stdin);
+				scanf("%d",&student[temp+i].eng);
+				continue;}
+			}
+	}
+	temp+=n;
+	for(i=0;i<temp;i++){
+		avg[i]=((float)(student[temp+i].math+student[temp+i].phy+student[temp+i].eng))/3;
+		avg_temp[i]=((float)(student[temp+i].math+student[temp+i].phy+student[temp+i].eng))/3;
+	}
+	for(i=0;i<temp;i++){
+		rank[i]=i;
+	}
+	for(i=0;i<temp;i++){
+		for(j=0;j<temp-1;j++){
+			if(avg_temp[j]<avg_temp[j+1]){
+				float_temp=avg_temp[j];
+				avg_temp[j]=avg_temp[j+1];
+				avg_temp[j+1]=float_temp;
+				int_temp=rank[j];
+				rank[j]=rank[j+1];
+				rank[j+1]=int_temp;			
+		}
+	}
+}
+void choise_b(void){
+	int i;
+	char ch;
+	printf("名子 id math phy eng avg_grade\n");
+	for(i=0;i<temp;i++){
+		printf("%-6s %-6s %3d %3d %3d %3.1f\n");
+	}
+	
+	
+}
+}
+
