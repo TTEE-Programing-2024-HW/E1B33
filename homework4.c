5960eb72c2580f070a3e3084ad6b641c00ef8433
張宸瑋 <u11202233@o365.ttu.edu.tw>
2024-06-01 08:39:59 +0800

    第一題完成

---
+++ homework4.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+		printf("-------------------\n");
+	printf("|                 |\n");
+	printf("|     系所:電機系 |\n");
+	printf("|                 |\n");
+	printf("|     班級:E1B    |\n");
+	printf("|                 |\n");
+	printf("|     座號:33     |\n");
+	printf("|                 |\n");
+	printf("|     姓名:張宸瑋 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    程式設計HW3  |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|                 |\n");
+	printf("|    2024/5/16    |\n");
+	printf("-------------------\n");
+	system("pause");//螢幕畫面暫停，等待使用者按任意按鍵 
+	system("cls");//清除畫面 
+		int password,count;
+			printf("輸入4個數字密碼:");
+	scanf("%d",&password);
+	for(;;){
+		if(count==2){
+			system("pause");
+			return 0;
+		}
+		if(password==2024){
+			break;
+		}else{
+		printf("錯誤再輸入一次密碼");
+		scanf("%d",&password);
+		count++;
+	}
+}
+	system("pause");
+	system("cls");
+}
